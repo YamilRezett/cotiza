0b4367fbd6715cedcf655027ca8dc595d5530ed4
Yamil Rezett <yamilrezett@gmail.com>
2022-06-21 17:01:02 -0300

    DOM aplicando JS al html

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
-const marca = prompt("Ingresa la marca del vehiculo");
-const año   = prompt("Ingresa el año del vehiculo");
-const valor = prompt("Ingrese valor del vehiculo");
-console.log(marca, año, valor)
+let enviar = document.getElementById("enviar")
+let datos = document.getElementById("datos")
+let borrar = document.getElementById("borrar")
 
-let operador;
+datos.hidden = "none"
 
-operador = Number(prompt("Ingrese la Cobertura:\n1 -basica\n2 -MX intermedia\n3 -Todo riesgo C/F de $20.000"))
+enviar.addEventListener("click", () =>{
+    datos.hidden=""
+    let marca = document.querySelector("#marca").value 
+    let año = document.querySelector("#año").value
+    let valor =document.querySelector("#valor").value;
 
-while(operador != 4){
-    valir1 = 1600
-    valor2 = 4000
-    valor3 = 10000
-    switch(operador){
-        case 1:
-            alert("La cobertura basica tiene un valor de "+ (valir1));
-            break;
-        case 2:
-            alert("La cobertura MX tiene un valor de "+ (valir1 + valor2));
-            break;
-        case 3:
-            alert("La cobertura T/R C/F de $20.000 tiene un valor de "+ (valir1 + valor3));
-            break;
-        default :
-            alert("Valor incorrecto");
-            break;       
-    }
-    operador = Number(prompt("Ingrese la Cobertura:\n1 -basica\n2 -MX intermedia\n3 -Todo riesgo C/F de $20.000\n4 -para salir"))
-}
+    const content = document.createElement("div")
 
-let Agregados = prompt("Ingresa las ampliaciones de tu cobertura basica o SALIR para terminar")
-let contacto
-let salida
- while (salida != "SALIR"){
-     alert("El prod ingresado es " + Agregados)
-     contacto = prompt("Ingrese un contacto y un asesor se comunicara a la breveda")
-     alert("Gracias por su visita")
-     salida = prompt("Ingrese SALIR para finalizar")
-     console.log(contacto + Agregados)
+    content.innerHTML =`
+    <h2>marca: ${marca}</h2>
+    <h2>año: ${año}</h2> 
+    <h2>año: ${valor}</h2> 
+    `;
+    datos.append(content)
+          })
+
+borrar.addEventListener("click", () =>{
+    datos.innerHTML = ""
+    datos.hidden = "none"
+})
+
+// const marca = prompt("Ingresa la marca del vehiculo");
+// const año   = prompt("Ingresa el año del vehiculo");
+// const valor = prompt("Ingrese valor del vehiculo");
+// console.log(marca, año, valor)
+
+// let operador;
+
+// operador = Number(prompt("Ingrese la Cobertura:\n1 -basica\n2 -MX intermedia\n3 -Todo riesgo C/F de $20.000"))
+
+// while(operador != 4){
+//     valir1 = 1600
+//     valor2 = 4000
+//     valor3 = 10000
+//     switch(operador){
+//         case 1:
+//             alert("La cobertura basica tiene un valor de "+ (valir1));
+//             break;
+//         case 2:
+//             alert("La cobertura MX tiene un valor de "+ (valir1 + valor2));
+//             break;
+//         case 3:
+//             alert("La cobertura T/R C/F de $20.000 tiene un valor de "+ (valir1 + valor3));
+//             break;
+//         default :
+//             alert("Valor incorrecto");
+//             break;       
+//     }
+//     operador = Number(prompt("Ingrese la Cobertura:\n1 -basica\n2 -MX intermedia\n3 -Todo riesgo C/F de $20.000\n4 -para salir"))
+// }
+
+// let Agregados = prompt("Ingresa las ampliaciones de tu cobertura basica o siguiente para pasar")
+// let contacto
+// let salida
+//  while (salida != "SALIR"){
+//      alert("El prod ingresado es " + Agregados)
+//      contacto = prompt("Ingrese un contacto y un asesor se comunicara a la breveda")
+//      alert("Gracias por su visita")
+//      salida = prompt("Ingrese SALIR para finalizar")
+//      console.log(contacto + Agregados)
     
-}
+// }
 
 let ControlDeProductos = [
     {nombre: "Cobertura1", precio: 1600},
